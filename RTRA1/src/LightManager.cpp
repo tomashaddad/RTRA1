7415360dd95c3a5fa8a128d05a704f6c9ced0045
Tomas Haddad <tomashaddad@gmail.com>
2021-09-05 01:35:43 +1000

    different colours

--- RTRA1/src/LightManager.cpp
+++ RTRA1/src/LightManager.cpp
@@ -6,30 +6,39 @@ LightManager::LightManager() {
 	// m_directional is updated per frame
 
 	float d = 5.0f;
+	float i = 0.8;
 
-	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(1.0, 1.0, 1.0),
-		glm::vec3(1.0, 1.0, 1.0), glm::vec3(d, d, d), 1.0f, 0.045, 0.0075);
+	// white
+	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(i, i, i),
+		glm::vec3(i, i, i), glm::vec3(d, d, d), 1.0f, 0.045, 0.0075);
 
-	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(1.0, 1.0, 1.0),
-		glm::vec3(1.0, 1.0, 1.0), glm::vec3(d, d, -d), 1.0f, 0.045, 0.0075);
+	// red
+	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(i, 0.0, 0.0),
+		glm::vec3(i, 0.0, 0.0), glm::vec3(d, d, -d), 1.0f, 0.045, 0.0075);
 
-	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(1.0, 1.0, 1.0),
-		glm::vec3(1.0, 1.0, 1.0), glm::vec3(d, -d, d), 1.0f, 0.045, 0.0075);
+	// yellow
+	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(i, i, 0.0),
+		glm::vec3(i, i, 0.0), glm::vec3(d, -d, d), 1.0f, 0.045, 0.0075);
 
-	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(1.0, 1.0, 1.0),
-		glm::vec3(1.0, 1.0, 1.0), glm::vec3(d, -d, -d), 1.0f, 0.045, 0.0075);
+	// pink
+	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(i, 0.0, i),
+		glm::vec3(i, 0.0, i), glm::vec3(d, -d, -d), 1.0f, 0.045, 0.0075);
 
-	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(1.0, 1.0, 1.0),
-		glm::vec3(1.0, 1.0, 1.0), glm::vec3(-d, d, d), 1.0f, 0.045, 0.0075);
+	// green
+	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(0.0, i, 0.0),
+		glm::vec3(0.0, i, 0.0), glm::vec3(-d, d, d), 1.0f, 0.045, 0.0075);
 
-	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(1.0, 1.0, 1.0),
-		glm::vec3(1.0, 1.0, 1.0), glm::vec3(-d, d, -d), 1.0f, 0.045, 0.0075);
+	// purple
+	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(0.5 * i, 0.0, i),
+		glm::vec3(0.5 * i, 0.0, i), glm::vec3(-d, d, -d), 1.0f, 0.045, 0.0075);
 
-	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(1.0, 1.0, 1.0),
-		glm::vec3(1.0, 1.0, 1.0), glm::vec3(-d, -d, d), 1.0f, 0.045, 0.0075);
+	// orange
+	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(i, 0.5 * i, 0.0),
+		glm::vec3(i, 0.5 * i, 0.0), glm::vec3(-d, -d, d), 1.0f, 0.045, 0.0075);
 
-	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(1.0, 1.0, 1.0),
-		glm::vec3(1.0, 1.0, 1.0), glm::vec3(-d, -d, -d), 1.0f, 0.045, 0.0075);
+	// blue
+	addPointLight(glm::vec3(0.1, 0.1, 0.1), glm::vec3(0.0, 0.0, i),
+		glm::vec3(0.0, 0.0, i), glm::vec3(-d, -d, -d), 1.0f, 0.045, 0.0075);
 }
 
 // Requires C++17 for struct-in-struct initialiser lists
